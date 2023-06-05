faaf94d46d8dabe393e08a1b058de499bcbff93b
Federico Centrone <Federico.centrone@hotmail.com>
2023-06-05 16:45:49 -0300

    commit pagina contacto terminada con js incluido

--- scripts/codigo.js
+++ scripts/codigo.js
@@ -1,3 +1,12 @@
-const primerTituloIndex=document.querySelector("h2")
 
-console.log(primerTituloIndex)
+// FUNCION SALUDAR EN EL FORMULARIO DE CONTACTO
+
+const inputBoton = document.querySelector('.acciones')
+
+
+inputBoton.addEventListener('click', saludo)
+
+
+function saludo () {
+    alert("¡Gracias por contactarnos!");
+}
